a1aca79a9104d918928463839ced29063bf7bf4f
demo <demo@localhost.localdomain>
2014-12-22 13:59:34 -0800

    Added writing to LMEM but blocks when all data has passed through capture kernel.

--- CPUCode/PacketCapture.c
+++ CPUCode/PacketCapture.c
@@ -13,26 +13,36 @@
 
 int main( int arc, char** argv )
 {
-	int data_len = 64;
+	int data_len = 192;
 	uint32_t* data_in = calloc(data_len, sizeof(*data_in));
-	uint32_t* data_out = calloc(data_len, sizeof(*data_in));
+	uint32_t* data_out = calloc(data_len, sizeof(*data_out));
 
 	for( int i=0; i<data_len; i++ )
 	{
 		data_in[i] = i;
-		data_out[i] = 0;
 	}
 
-	PacketCapture(data_len, data_in, data_out);
+	max_file_t *maxfile = PacketCapture_init();
+	int burstSizeInBytes = max_get_burst_size(maxfile, "cmd_toMem");
 
-	for( int i=0; i<data_len; i++ )
-	{
-		if( data_out[i] != data_in[i] )
-		{
-			printf("data_out[%d](%"PRIx32") != data_in[%d](%"PRIx32")\n", i, data_out[i], i, data_in[i]);
-			return EXIT_FAILURE;
-		}
-	}
+	printf("burstSize: %d\n", burstSizeInBytes);
+	printf("burstCount: %d\n", (data_len * sizeof(*data_in))/burstSizeInBytes);
+
+	printf("Running capture.\n");
+	PacketCapture_capture(burstSizeInBytes, data_len, data_in);
+
+
+//	printf("Running read.\n");
+//	PacketCapture_read(data_len, data_out);
+//
+//	for( int i=0; i<data_len; i++ )
+//	{
+//		if( data_out[i] != data_in[i] )
+//		{
+//			printf("data_out[%d](%"PRIx32") != data_in[%d](%"PRIx32")\n", i, data_out[i], i, data_in[i]);
+//			return EXIT_FAILURE;
+//		}
+//	}
 
 	printf("Check passed.\n");
 
