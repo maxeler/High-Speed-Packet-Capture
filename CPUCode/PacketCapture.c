82ebe8f5c645fcf302b869063c9568e1ff3b8706
Mike Wilson <mwilson@maxeler.com>
2014-12-22 16:31:40 -0800

    Added tail/head support for blocking input when mem full.

--- CPUCode/PacketCapture.c
+++ CPUCode/PacketCapture.c
@@ -23,26 +23,28 @@ int main( int arc, char** argv )
 	}
 
 	max_file_t *maxfile = PacketCapture_init();
-	int burstSizeInBytes = max_get_burst_size(maxfile, "cmd_toMem");
+	int burst_size = max_get_burst_size(maxfile, "cmd_toMem");
+	int mem_size_bursts = (data_len * sizeof(*data_in)) / burst_size;
+	uint64_t tail_addr = 0;
 
-	printf("burstSize: %d\n", burstSizeInBytes);
-	printf("burstCount: %d\n", (data_len * sizeof(*data_in))/burstSizeInBytes);
+	printf("burstSize: %d\n", burst_size);
+	printf("burstCount: %d\n", (data_len * sizeof(*data_in))/burst_size);
 
 	printf("Running capture.\n");
-	PacketCapture_capture(burstSizeInBytes, data_len, data_in);
+	PacketCapture_capture(burst_size, data_len, mem_size_bursts, tail_addr, data_in);
 
 
-//	printf("Running read.\n");
-//	PacketCapture_read(data_len, data_out);
-//
-//	for( int i=0; i<data_len; i++ )
-//	{
-//		if( data_out[i] != data_in[i] )
-//		{
-//			printf("data_out[%d](%"PRIx32") != data_in[%d](%"PRIx32")\n", i, data_out[i], i, data_in[i]);
-//			return EXIT_FAILURE;
-//		}
-//	}
+	printf("Running read.\n");
+	PacketCapture_read(data_len, data_out);
+
+	for( int i=0; i<data_len; i++ )
+	{
+		if( data_out[i] != data_in[i] )
+		{
+			printf("data_out[%d](%"PRIx32") != data_in[%d](%"PRIx32")\n", i, data_out[i], i, data_in[i]);
+			return EXIT_FAILURE;
+		}
+	}
 
 	printf("Check passed.\n");
 
