be78af3d2ab0a003446902c2f630c2207293952a
Michael Wilson <mwilson@maxeler.com>
2015-02-06 12:40:51 -0800

    Add sending lmem contents to network client.

--- CPUCode/PacketCapture.c
+++ CPUCode/PacketCapture.c
@@ -16,22 +16,69 @@
 
 static int CHUNK_SIZE = 16;
 static int BURST_SIZE = 192;
+static int SERVER_PORT = 2511;
+
+static void dfe_configure_interface( max_engine_t* engine, max_net_connection_t interface, const char* ip, const char* netmask )
+{
+	assert(engine != NULL);
+	assert(ip != NULL);
+	assert(netmask != NULL);
+
+	struct in_addr ip_addr;
+	struct in_addr netmask_addr;
+	inet_aton(ip, &ip_addr);
+	inet_aton(netmask, &netmask_addr);
+
+	max_ip_config(engine, interface, &ip_addr, &netmask_addr);
+}
+
+static max_tcp_socket_t* dfe_create_sockets( max_engine_t* engine, const char* stream, const char* ip, int port )
+{
+	assert(engine != NULL);
+
+	max_tcp_socket_t* socket = max_tcp_create_socket(engine, stream);
+
+	struct in_addr ip_addr;
+	inet_aton(ip, &ip_addr);
+
+	max_tcp_connect(socket, &ip_addr, port);
+
+	printf("Waiting for socket state: MAX_TCP_STATE_ESTABLISHED\n");
+	max_tcp_await_state(socket, MAX_TCP_STATE_ESTABLISHED, NULL);
+
+	return socket;
+}
 
 int main( int argc, char** argv )
 {
-
 	(void) argc;
 	(void) argv;
 
-//	max_file_t *maxfile = PacketCapture_init();
+	max_file_t *maxfile = PacketCapture_init();
+	printf("maxload start\n");
+	max_engine_t * engine = max_load(maxfile, "*");
+	printf("maxload end\n");
 
 	int data_bursts = 1;
 	int data_size = data_bursts * BURST_SIZE;
-	int frames_len = data_size / CHUNK_SIZE;
+	uint64_t frames_len = data_size / CHUNK_SIZE;
 
 	FILE* file = fopen("./capture.pcap", "w+");
 	pcap_t* pcap = pcap_create(file, 0, 1, 0, 65535);
 	assert(pcap != NULL);
+
+	/* init */
+	dfe_configure_interface(engine, MAX_NET_CONNECTION_QSFP_TOP_10G_PORT2, "5.5.5.2", "255.255.255.0");
+	max_tcp_socket_t* socket = dfe_create_sockets(engine, "tcpServerStream", "5.5.5.1", SERVER_PORT);
+	uint8_t socket_num = max_tcp_get_socket_number(socket);
+
+	PacketCapture_configSend_actions_t config_send_action =
+	{
+		.param_socket = socket_num,
+	};
+	PacketCapture_configSend_run(engine, &config_send_action);
+
+	PacketCapture_capture_run(engine, NULL);
 
 	/* read */
 	pcap_frame_t* frame = NULL;
@@ -40,9 +87,14 @@ int main( int argc, char** argv )
 		printf("Running read.\n");
 
 
-		printf("Reading %d frames (%dB)\n", frames_len, data_size);
+		printf("Reading %"PRIu64" frames (%dB)\n", frames_len, data_size);
 		uint64_t* data = malloc(data_size);
-		PacketCapture_read(data_bursts, data);
+		PacketCapture_read_actions_t read_action =
+		{
+			.param_bursts = data_bursts,
+			.outstream_toCpu = data,
+		};
+		PacketCapture_read_run(engine, &read_action);
 		printf("Done reading.\n");
 
 		int data_len = data_size / sizeof(*data);
@@ -79,11 +131,6 @@ int main( int argc, char** argv )
 			int eof = frame_part_get_eof(part);
 			if( sof )
 			{
-				if( frame != NULL )
-				{
-					pcap_frame_free(frame);
-					frame = NULL;
-				}
 				frame = pcap_frame_init(pcap, time(NULL), 0);
 				assert(frame != NULL);
 			}
@@ -97,6 +144,10 @@ int main( int argc, char** argv )
 			if( eof )
 			{
 				pcap_frame_write(pcap, frame);
+
+				// free
+				pcap_frame_free(frame);
+				frame = NULL;
 				fflush(file);
 			}
 
